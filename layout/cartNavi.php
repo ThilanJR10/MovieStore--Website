b01bccd5354fba2cd1770eaf95d236eb5d214752
ThilanLasitha <thilanlasitha95@gmail.com>
2017-04-22 23:58:29 +0530

    Add files via upload

---
+++ layout/cartNavi.php
@@ -0,0 +1,5 @@
+<!--<div od="cartNavi">
+	<button class="cartNavigation"><a href="#">Browse Categories</a></button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<button class="cartNavigation"><a href="#">View Cart</a></button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<button class="cartNavigation"><a href="#">Track Progress Of a Transaction</a></button>
+</div>-->
